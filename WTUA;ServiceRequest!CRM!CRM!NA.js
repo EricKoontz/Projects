0d0c245e99ad57015afcc9348e80f8b60f509667
JasonSLS <jason@esilverliningsolutions.com>
2018-09-24 11:48:16 -0700

    Delete WTUA;ServiceRequest!CRM!CRM!NA.js

--- WTUA;ServiceRequest!CRM!CRM!NA.js
+++
@@ -1,8 +0,0 @@
-//CRM INTEGRATION BEGIN - DO NOT CHANGE
-//include INCLUDES_CRM functions for CRM integration
-showDebug = true;
-eval(getScriptText("INCLUDES_CRM", null, false));
-logDebug("***** BEGIN CRM INTEGRATION *****");
-processShadowRecord(capId, wfTask, wfStatus, wfComment);
-logDebug("*****   END CRM INTEGRATION *****");
-//CRM INTEGRATION END
