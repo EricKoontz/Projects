99bb3373e8a2e5b4bb8d80b96aa002aab412a332
JasonSLS <jason@esilverliningsolutions.com>
2018-09-24 11:48:52 -0700

    Delete WTUA;Water!Water!SWMP!Permit.js

--- WTUA;Water!Water!SWMP!Permit.js
+++
@@ -1,7 +0,0 @@
-script398_ScheduleFinalInspection();
-
-//script 397
-logDebug("Script 397 Starting");
-if(ifTracer(wfTask == "Closure" && wfStatus == "Closed", "wfTask == Closure && wfStatus == Closed")) {
-	include("397_createPpbmpRecordBasedOnCustomListData");
-}
